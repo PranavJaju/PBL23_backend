f2c1ca60408054c4cf7818b33b5f9009efbbdbef
PranavJaju <pranavjaju45@gmail.com>
2023-05-20 19:41:30 +0530

    Cookies 3

--- server/middlewares/CAuth.js
+++ server/middlewares/CAuth.js
@@ -2,8 +2,23 @@ const Collector = require("../models/collector");
 const jwt = require("jsonwebtoken");
 const AuthCollector = async(req,res,next)=>{
    try{
-    const token = req.cookie.jwt;
-    const verifyUser = jwt.verify(token,process.env.JWT_KEY);
+    const token = req.cookies.jwt; // Assuming the token is stored in a cookie named "jwt"
+
+    if (!token) {
+      throw new Error('Authentication token not found');
+    }
+
+    let verifyUser;
+    try {
+      verifyUser = jwt.verify(token, process.env.JWT_KEY);
+    } catch (err) {
+      throw new Error('Invalid token');
+    }
+
+    if (!verifyUser._id) {
+      throw new Error('Invalid token');
+    }
+
     req.user = await Collector.findOne({_id:verifyUser._id});
     next();
    }catch(err){

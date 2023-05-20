95308702e005182ffa1ab8e36a361f8a3c95501f
PranavJaju <pranavjaju45@gmail.com>
2023-05-20 22:23:31 +0530

    Cookies Done

--- server/middlewares/ShopAuth.js
+++ server/middlewares/ShopAuth.js
@@ -3,8 +3,22 @@ const Provider = require("../models/provider");
 const jwt = require("jsonwebtoken");
 const AuthProvider = async(req,res,next)=>{
    try{
-    const token = req.cookie.jwt;
-    const verifyUser = jwt.verify(token,process.env.JWT_KEY);
+      const token = req.cookies.jwt; // Assuming the token is stored in a cookie named "jwt"
+
+      if (!token) {
+        throw new Error('Authentication token not found');
+      }
+  
+      let verifyUser;
+      try {
+        verifyUser = jwt.verify(token, process.env.JWT_KEY);
+      } catch (err) {
+        throw new Error('Invalid token');
+      }
+  
+      if (!verifyUser._id) {
+        throw new Error('Invalid token');
+      }
     req.user = await Provider.findOne({_id:verifyUser._id});
     next();
    }catch(err){

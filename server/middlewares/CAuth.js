08b30c0b304bcf5e8fc1f1d3027f572175d34474
PranavJaju <pranavjaju45@gmail.com>
2023-05-20 19:13:44 +0530

    Cookies 2

--- server/middlewares/CAuth.js
+++ server/middlewares/CAuth.js
@@ -2,7 +2,7 @@ const Collector = require("../models/collector");
 const jwt = require("jsonwebtoken");
 const AuthCollector = async(req,res,next)=>{
    try{
-    const token = req.header("Authorization").replace("Bearer ", "");
+    const token = req.cookie.jwt;
     const verifyUser = jwt.verify(token,process.env.JWT_KEY);
     req.user = await Collector.findOne({_id:verifyUser._id});
     next();
--- server/middlewares/ShopAuth.js
+++ server/middlewares/ShopAuth.js
@@ -3,7 +3,7 @@ const Provider = require("../models/provider");
 const jwt = require("jsonwebtoken");
 const AuthProvider = async(req,res,next)=>{
    try{
-    const token = req.header("Authorization").replace("Bearer ", "");
+    const token = req.cookie.jwt;
     const verifyUser = jwt.verify(token,process.env.JWT_KEY);
     req.user = await Provider.findOne({_id:verifyUser._id});
     next();

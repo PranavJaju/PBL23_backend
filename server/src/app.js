b1b20bfc75b334f0db9cc31f467360e11ffd44e0
Mihir <34931141+MihirDesh@users.noreply.github.com>
2023-05-21 16:40:04 +0530

    nodemailer integrated

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const cookieParser = require('cookie-parser')
 const cors = require("cors")
 const port = process.env.PORT || 5000;
 const dotenv = require("dotenv")
@@ -8,12 +7,13 @@ dotenv.config()
 require("../db/connection");
 const PRoutes = require("../routes/ProviderRoutes");
 const CRoutes = require("../routes/CollectorRoutes");
+const MailRoute = require("../routes/Mail");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser())
-app.use(cors());
 app.use(CRoutes);
+app.use(cors());
 app.use(PRoutes);
+app.use(MailRoute);
 app.listen(port,()=>{
     console.log(`Connected to port : ${port}`);
 })
